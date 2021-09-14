efab864ecb1f041950bb6854763aebc7b156237d
Minho Sung <misung@c6r10s2.42seoul.kr>
2021-09-15 01:52:18 +0900

    ex12 needed

--- ex11/ft_putstr_with_non_printable.c
+++ ex11/ft_putstr_with_non_printable.c
@@ -6,9 +6,35 @@
 /*   By: misung <misung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 20:16:06 by misung            #+#    #+#             */
-/*   Updated: 2021/09/14 20:24:35 by misung           ###   ########.fr       */
+/*   Updated: 2021/09/15 01:27:58 by misung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_is_printable(unsigned char c)
+{
+	if (c >= 32 && c != 127)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+void	ft_print_non_printable_char(unsigned char c)
+{
+	ft_putchar('\\');
+	ft_putchar("0123456789abcdef"[c / 16]);
+	ft_putchar("0123456789abcdef"[c % 16]);
+}
 
 void	ft_putstr_non_printable(char *str)
 {
@@ -17,7 +43,14 @@ void	ft_putstr_non_printable(char *str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-
+		if (ft_is_printable(str[i]) == 1)
+		{
+			ft_putchar(str[i]);
+		}
+		else
+		{
+			ft_print_non_printable_char(str[i]);
+		}
 		i++;
 	}
 }
--- ex12/ft_print_memory.c
+++ ex12/ft_print_memory.c
@@ -1,34 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_print_memory.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: misung <misung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/14 19:34:04 by misung            #+#    #+#             */
-/*   Updated: 2021/09/15 01:06:38 by misung           ###   ########.fr       */
+/*   Created: 2021/09/15 01:51:46 by misung            #+#    #+#             */
+/*   Updated: 2021/09/15 01:51:48 by misung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
-{
-	unsigned int	src_size;
-	unsigned int	i;
 
-	src_size = 0;
-	i = 0;
-	while (src[src_size] != '\0')
-	{
-		src_size++;
-	}	
-	if (size != 0)
-	{
-		while (src[i] != '\0' && i < (size - 1))
-		{
-			dest[i] = src[i];
-			i++;
-		}
-		dest[i] = '\0';
-	}
-	return (src_size);
-}
