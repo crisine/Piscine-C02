aa7b8b492f14292327e3067bfe1cddcb13c54b97
Minho Sung <misung@c6r10s2.42seoul.kr>
2021-09-21 06:20:57 +0900

    ex09 modified

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -5,44 +5,62 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: misung <misung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/14 18:15:52 by misung            #+#    #+#             */
-/*   Updated: 2021/09/14 19:26:09 by misung           ###   ########.fr       */
+/*   Created: 2021/09/16 18:18:47 by misung            #+#    #+#             */
+/*   Updated: 2021/09/21 00:15:58 by misung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_is_not_alphabet(char c)
+#include <stdbool.h>
+
+bool	ft_is_alphabet(char c)
 {
 	if ((c >= 'a' && c <= 'z')
 		|| (c >= 'A' && c <= 'Z'))
 	{
-		return (0);
+		return (true);
 	}
-	return (1);
+	return (false);
 }
 
-char	ft_char_judge(char c, int *b_first_char)
+bool	ft_is_numeric(char c)
 {
-	if (*b_first_char == 1)
+	if (c >= '0' && c <= '9')
 	{
-		if (c >= 'a' && c <= 'z')
-		{
-			c -= 32;
-			*b_first_char = 0;
-		}
-		else if (c >= 'A' && c <= 'Z')
-		{
-			*b_first_char = 0;
-		}
+		return (true);
 	}
-	else if (*b_first_char == 0)
+	return (false);
+}
+
+bool	ft_is_lowercase(char c)
+{
+	if (c >= 'a' && c <= 'z')
 	{
-		if (c >= 'A' && c <= 'Z')
+		return (true);
+	}
+	return (false);
+}
+
+char	ft_change_letter(char c, bool *is_first_char)
+{
+	if (*is_first_char && ft_is_alphabet(c))
+	{
+		if (ft_is_lowercase(c))
+			c -= 32;
+		*is_first_char = false;
+	}
+	else
+	{
+		if (!(ft_is_numeric(c)) && !(ft_is_alphabet(c)))
+		{
+			*is_first_char = true;
+		}
+		else if (ft_is_alphabet(c) && !(ft_is_lowercase(c)))
 		{
 			c += 32;
 		}
-		else if (ft_is_not_alphabet(c) == 1)
+		else
 		{
-			*b_first_char = 1;
+			*is_first_char = false;
 		}
 	}
 	return (c);
@@ -50,14 +68,14 @@ char	ft_char_judge(char c, int *b_first_char)
 
 char	*ft_strcapitalize(char *str)
 {
-	int				b_first_char;
-	unsigned int	i;
+	int		i;
+	bool	is_first_char;
 
-	b_first_char = 1;
 	i = 0;
+	is_first_char = true;
 	while (str[i] != '\0')
 	{
-		str[i] = ft_char_judge(str[i], &b_first_char);
+		str[i] = ft_change_letter(str[i], &is_first_char);
 		i++;
 	}
 	return (str);
--- ex12/ft_print_memory.c
+++ ex12/ft_print_memory.c
@@ -6,8 +6,20 @@
 /*   By: misung <misung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 01:51:46 by misung            #+#    #+#             */
-/*   Updated: 2021/09/15 01:51:48 by misung           ###   ########.fr       */
+/*   Updated: 2021/09/15 15:44:55 by misung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+
+void	*ft_print_memory(void *addr, unsigned int size)
+{
+	char	c_addr_idx;
+
+	c_addr_idx = (char*) addr;
+	while ((void*) c_addr_idx < (addr + size))
+	{
+		
+	}
+	return (addr);
+}
